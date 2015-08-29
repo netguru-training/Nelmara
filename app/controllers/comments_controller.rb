f9b3210a81f05aa7ca7e27f451c27a2795a4e94d
dajvido <dawid.jaskot@gmail.com>
2015-08-29 14:28:18 +0200

    Add destroy controller to comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,11 +1,8 @@
 class CommentsController < ApplicationController
-  before_action :set_comment, only: [:show, :edit, :update, :destroy]
   expose(:post)
   expose(:comments, ancestor: :post)
   expose(:comment, attributes: :comment_params)
 
-  # POST /comments
-  # POST /comments.json
   def create
     if comment.save
       redirect_to post, notice: 'Comment was successfully created.'
@@ -14,23 +11,19 @@ class CommentsController < ApplicationController
     end
   end
 
-  def update
-    if comment.update(comment_params)
-      redirect_to comment, notice: 'Comment was successfully updated.'
+  def destroy
+    comment.body = t('comment.destroy_msg', user: current_user.username)
+    if comment.save
+      redirect_to post_url(post), notice: 'Comment was successfully destroyed.'
     else
-      render :edit
+      redirect_to post_url(post)
     end
   end
 
-  # DELETE /comments/1
-  def destroy
-    comment.destroy
-    redirect_to comments_url, notice: 'Comment was successfully destroyed.'
-  end
-
   private
-    # Never trust parameters from the scary internet, only allow the white list through.
+
     def comment_params
       params.require(:comment).permit(:body, :post_id)
     end
 end
+
