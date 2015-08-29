2c43c6c486c2696ac5807693a089fb03bcda2180
mzdunek93 <mzdunek93@gmail.com>
2015-08-29 11:48:04 +0200

    Fix indentation in Post factory

--- spec/factories/Posts.rb
+++ spec/factories/Posts.rb
@@ -1,10 +1,9 @@
 FactoryGirl.define do
   factory :post do
     title "MyString"
-url "MyString"
-body "MyText"
-type 1
-user nil
+    url "MyString"
+    body "MyText"
+    type 1
+    user nil
   end
-
 end
