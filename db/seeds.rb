d8508a9fbf7e869858c32e2ec2a672ac4a28e9f8
Nataly Tkachuk <techgirlua@gmail.com>
2015-08-29 18:07:41 +0300

    Add seeds data on User, Post and Comment

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,33 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+puts "Seeds: start"
+
+User.create!(email: 'admin@admin.com',password: 'adminadmin', username: 'admin')
+
+5.times do
+  User.create!(
+    username: Faker::Internet.user_name,
+    email: Faker::Internet.email,
+    password: Faker::Internet.password
+    )
+end
+
+users = User.all
+
+20.times do
+  user = users.sample
+  Post.create!(
+    body: Faker::Hacker.say_something_smart,
+    title: Faker::Hacker.verb + Faker::Hacker.adjective + Faker::Haker.noun,
+    user: user,
+    image: Faker::Avatar.image
+    )
+(0..7).times do
+  Comment.create!(
+    body: Faker::Lorem.sentence,
+    user: users.sample
+      )
+  end
+
+
+end
+
+puts "Seeds: done"
