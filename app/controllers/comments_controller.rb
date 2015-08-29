88640f3f9a01416de081ddefd79e53318cb74b03
Nataly Tkachuk <techgirlua@gmail.com>
2015-08-29 14:15:38 +0300

    Remove json format c3bf4c725cca40a239980edca586c3cf75599315

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -7,39 +7,25 @@ class CommentsController < ApplicationController
   # POST /comments
   # POST /comments.json
   def create
-    respond_to do |format|
-      if comment.save
-        format.html { redirect_to post, notice: 'Comment was successfully created.' }
-        format.json { render :show, status: :created, location: @comment }
-      else
-        format.html { render :new }
-        format.json { render json: comment.errors, status: :unprocessable_entity }
-      end
+    if comment.save
+      redirect_to post, notice: 'Comment was successfully created.'
+    else
+      render :new
     end
   end
 
-  # PATCH/PUT /comments/1
-  # PATCH/PUT /comments/1.json
   def update
-    respond_to do |format|
-      if comment.update(comment_params)
-        format.html { redirect_to comment, notice: 'Comment was successfully updated.' }
-        format.json { render :show, status: :ok, location: comment }
-      else
-        format.html { render :edit }
-        format.json { render json: comment.errors, status: :unprocessable_entity }
-      end
+    if comment.update(comment_params)
+      redirect_to comment, notice: 'Comment was successfully updated.'
+    else
+      render :edit
     end
   end
 
   # DELETE /comments/1
-  # DELETE /comments/1.json
   def destroy
     comment.destroy
-    respond_to do |format|
-      format.html { redirect_to comments_url, notice: 'Comment was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    redirect_to comments_url, notice: 'Comment was successfully destroyed.'
   end
 
   private
