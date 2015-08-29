27be7683e45d679310aef67b248bc9f710f75304
Nataly Tkachuk <techgirlua@gmail.com>
2015-08-29 17:04:57 +0300

    Add finish_signup for device-omniauth-facebook

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,55 @@
+class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+
+  # GET /users/:id.:format
+  def show
+    # authorize! :read, @user
+  end
+
+  # GET /users/:id/edit
+  def edit
+    # authorize! :update, @user
+  end
+
+  # PATCH/PUT /users/:id.:format
+  def update
+    if @user.update(user_params)
+      sign_in(@user == current_user ? @user : current_user, :bypass => true)
+      redirect_to @user, notice: 'Your profile was successfully updated.'
+    else
+      render action: 'edit'
+    end
+  end
+
+  # GET/PATCH /users/:id/finish_signup
+  def finish_signup
+    # authorize! :update, @user
+    if request.patch? && params[:user] #&& params[:user][:email]
+      if @user.update(user_params)
+        @user.skip_reconfirmation!
+        sign_in(@user, :bypass => true)
+        redirect_to @user, notice: 'Your profile was successfully updated.'
+      else
+        @show_errors = true
+      end
+    end
+  end
+
+  # DELETE /users/:id.:format
+  def destroy
+    # authorize! :delete, @user
+    @user.destroy
+    redirect_to root_url }
+  end
+
+  private
+    def set_user
+      @user = User.find(params[:id])
+    end
+
+    def user_params
+      accessible = [ :name, :email ] # extend with your own params
+      accessible << [ :password, :password_confirmation ] unless params[:user][:password].blank?
+      params.require(:user).permit(accessible)
+    end
+end
