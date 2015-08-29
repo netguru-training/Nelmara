3dce61ff1f8d37548ff948085b0c37028b496edb
dajvido <dawid.jaskot@gmail.com>
2015-08-29 15:52:39 +0200

    Add search_by_title to Post using pg_search with dictionary matching

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,11 @@ class Post < ActiveRecord::Base
   belongs_to :user
   has_many :comments, dependent: :destroy
 
+  include PgSearch
+  pg_search_scope :kinda_matching, against: :title, using: {
+                                                      tsearch: {dictionary: "english"}
+                                                    }
+
   default_scope { order(created_at: :desc) }
 
   acts_as_votable
@@ -11,8 +16,13 @@ class Post < ActiveRecord::Base
   validates_presence_of :title
   validates :body, presence: true, length: 2..300
 
+  def self.search_by_title(query)
+    kinda_matching(query)
+  end
+
   def total_votes
     votes_for.up.size - votes_for.down.size
   end
+
 end
 
