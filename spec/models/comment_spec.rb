7ef69ecd2204a65d64cf33d876726306ebaa29cf
dajvido <dawid.jaskot@gmail.com>
2015-08-29 14:03:49 +0200

    Add validations for Comment body

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'associations' do
+    it { should belong_to(:post) }
+  end
+
+  describe 'db columns' do
+    it { should have_db_column(:body) }
+  end
+
+  describe 'validations' do
+    it { should validate_presence_of(:body) }
+    it { should validate_length_of(:body).is_at_least(2).is_at_most(140) }
+  end
+
 end
+
