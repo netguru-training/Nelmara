d022225e8897e0a5033ccca5fcd7ac356dcbcb70
Nataly Tkachuk <techgirlua@gmail.com>
2015-08-29 15:46:44 +0300

    Add identity model for device-omniauth-facebook

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,16 +2,12 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable, :omniauthable
 
   has_many :posts
   has_many :comments
 
   validates_presence_of :username
-
-  def self.find_for_oauth(auth)
-    find_or_create_by(uid: auth.uid, provider: auth.provider)
-  end
 
 end
 
