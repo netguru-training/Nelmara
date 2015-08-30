c23113fea80aa7836b728b776998bbd1652d76bf
Nataly Tkachuk <techgirlua@gmail.com>
2015-08-30 12:18:58 +0300

    Configure factory_girl, capybara and add feature spec

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,8 @@
 FactoryGirl.define do
   factory :user do
-    
+    email { Faker::Internet.safe_email }
+    password { Faker::Internet.password }
+    username { Faker::Internet.user_name }
   end
 
 end
